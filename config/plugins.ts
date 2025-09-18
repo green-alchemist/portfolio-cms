ea22fd739b8eb20d6af8fd864c3ef0cf876f6045
green-alchemist <kyleconley2@gmail.com>
2025-09-18 01:13:59 -0700

    we're working

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,24 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET_KEY'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET_NAME'),
+        },
+      },
+      // These parameters could solve issues with ACL public-read access via AWS account settings
+      actionOptions: {
+        upload: {
+          ACL: null,
+        },
+        uploadStream: {
+          ACL: null,
+        },
+      },
+    },
+  },
+});
--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
-  host: env('HOST', '127.0.0.1'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'http://localhost:1337'), // Important for production
   app: {
     keys: env.array('APP_KEYS'),
   },
@@ -8,3 +9,4 @@ export default ({ env }) => ({
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
+
