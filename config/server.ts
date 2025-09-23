4cbc295f02e3ef123690586655ca6e2a1e883b95
green-alchemist <kyleconley2@gmail.com>
2025-09-22 18:14:16 -0700

    stray dog

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('NODE_ENV') == 'development' ?  `http://${env('HOST')}:${env('PORT')}` : `https://${env('PUBLIC_URL')})`, // Important for production
+  url: env('NODE_ENV') == 'development' ?  `http://${env('HOST')}:${env('PORT')}` : `https://${env('PUBLIC_URL')}`, // Important for production
   app: {
     keys: env.array('APP_KEYS'),
   },
